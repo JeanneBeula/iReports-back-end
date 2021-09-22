e640f5bea2e8ca9be7aa40464a734c6ab42ec95c
JeanneBeula <nzabihijanebeula@gmail.com>
2021-09-22 11:20:10 +0200

    ft-getUser
    check whether the array is not empty
    display all users within the array

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { hashPassword, compared } = require("../helper/bcrypt.utils");
 const userDb = require("../models/user");
-const {generateToken} = require("../helper/jwt");
+const { generateToken } = require("../helper/jwt");
+const { v4 } = require("uuid");
 
 class User {
   static async registerUser(req, res) {
@@ -12,7 +13,7 @@ class User {
         return res.status(409).json({ error: "The user email already exist" });
       }
       const newUser = {
-        id: userDb.length + 1,
+        id: v4(),
         firstName,
         lastName,
         email,
@@ -20,7 +21,6 @@ class User {
       };
       userDb.push(newUser);
       const token = generateToken(newUser.email, newUser.id);
-      console.log(">>>>>>>>>>>>>>>",token);
       const userInfo = { ...newUser, token };
       delete userInfo.password;
       return res
@@ -34,6 +34,7 @@ class User {
     try {
       const { email, password } = req.body;
       const user = userDb.find((e_user) => e_user.email === email);
+      const token = generateToken(user.email, user.id);
 
       if (!user) {
         return res
@@ -47,7 +48,19 @@ class User {
       }
       return res
         .status(200)
-        .json({ message: "you  have logged in successfully" });
+        .json({ message: "you  have logged in successfully", token });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async getUsers(req, res) {
+    try {
+      if (userDb.length <= 0) {
+        return res.status(404).json({ status: 404, error: "user not found" });
+      }
+      return res
+        .status(200)
+        .json({ status: 200, message: "List of Users", userDb });
     } catch (error) {
       return res.status(500).json(error.message);
     }
